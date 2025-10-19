c063d5ac6bac14e5511d9611a5bc914ceaa259fd
aoi_takenaka <aoitakenaka.wk@gmail.com>
2025-10-19 20:11:23 +0900

    [Add]アクティビティとリスナ

--- HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.hellosample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +15,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btClick = findViewById<Button>(R.id.btClick)
+        val listener = HelloListener()
+        btClick.setOnClickListener(listener)
+
+        val btClear = findViewById<Button>(R.id.btClear)
+        btClear.setOnClickListener(listener)
+    }
+
+    // プライベートインナークラスとして登録
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            when(view.id) {
+                R.id.btClick -> {
+                    val inputStr = input.text.toString()
+                    output.text = inputStr + "さん、こんにちわ"
+                }
+                R.id.btClear -> {
+                    input.setText("")
+                    output.text = ""
+                }
+            }
+
+        }
     }
 }
