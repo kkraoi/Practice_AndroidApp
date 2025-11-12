01db753e0a259c55b023b124c7923726e758634a
aoi_takenaka <aoitakenaka.wk@gmail.com>
2025-11-13 08:28:35 +0900

    add:コーディネイトレイアウト作成

---
+++ CoordinatorLayoutSample/app/src/main/java/com/example/coordinatorlayoutsample/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.coordinatorlayoutsample
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // ツールバーにロゴを設定
+        binding.toolbar.setLogo(R.mipmap.ic_launcher) // 1
+
+        // ツールバーにタイトル文字列流を設定
+        binding.toolbar.setTitle(R.string.toolbar_title) // 2
+
+        // ツールバーのタイトル文字色を設定
+        binding.toolbar.setTitleTextColor(Color.WHITE) // 3
+
+        // ツールバーのサブタイトルを設定
+        binding.toolbar.setSubtitle(R.string.toolbar_subtitle) // 4
+
+        // サブタイトルの文字色を設定
+        binding.toolbar.setSubtitleTextColor(Color.LTGRAY) // 5
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
