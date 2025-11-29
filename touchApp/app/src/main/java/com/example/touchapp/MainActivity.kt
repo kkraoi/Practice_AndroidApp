0d8c7474894ad84189f15ce7cdc9dbe81e1c4bf5
aoi_takenaka <aoitakenaka.wk@gmail.com>
2025-11-29 21:49:48 +0900

    [update]画面遷移サンプル

---
+++ touchApp/app/src/main/java/com/example/touchapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.touchapp
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+         setContentView(R.layout.activity_main)
+
+        val et: EditText = findViewById(R.id.et)
+        val btnStart: Button = findViewById(R.id.btnStart)
+
+        // 画面遷移
+        btnStart.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("MY_NAME", et.text.toString())
+
+            startActivity(intent)
+        }
+
+    }
+}
--- touchApp/app/src/main/java/com/example/touchapp/SecondActivity.kt
+++ touchApp/app/src/main/java/com/example/touchapp/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.touchapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvName: TextView = findViewById(R.id.tvName)
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        val my_name = intent.getStringExtra("MY_NAME")
+        tvName.text = my_name + "さん"
+
+        btnBack.setOnClickListener {
+            finish()
         }
     }
 }
